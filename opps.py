3921c47a1ebcc05e56aeb0be7aec7e88f4c33e79
shikhasharma-passion <shikha.c.@zhi.org.in>
2026-01-09 21:48:37 +0530

    adding oops program

---
+++ opps.py
@@ -0,0 +1,12 @@
+class details:
+    name = "shikha"
+    age = 22
+    ciity = "pune"
+    country = "India"
+    profession = "engineer"
+
+obj1 = details()
+print("my name is",obj1.name, "and I am from",obj1.ciity)
+print(obj1.age)
+print(obj1.profession)
+

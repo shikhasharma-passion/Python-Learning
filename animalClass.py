cbec57c097b67b39370bd1932284ecf59b6c7443
Shikha Sharma <144619810+shikhasharma-passion@users.noreply.github.com>
2026-01-11 12:34:56 +0530

    Refactor animal class and add instances
    Refactored animal class to use a constructor and added multiple animal instances.

--- animalClass.py
+++ animalClass.py
@@ -1,20 +1,34 @@
+# class define
 class animal:
-   name = "lion"
-   name2 = "wolf"
-   name3 = "tiger"
-   name4 = "crocodile"
-   habit = "eat flesh"
-   typeOfAnimal = "carnivour"
 
-   def info(self):
-    # {a}, {b}, {c}" normal string formatting
-    print(f"{self.name}, {self.name2}, {self.name3}, {self.name4} {self.habit} so, they are called {self.typeOfAnimal} animals")
-    # "{a, b, c}"  become tuple
-    print(f"{self.name, self.name2, self.name3, self.name4 } {self.habit} so, they are called {self.typeOfAnimal} animals")
+   # constractor (parameterized)
+   def __init__(self, imgOfAnimal, habit, animalType):
+     self.imgOfAnimal = imgOfAnimal
+     self.habit = habit
+     self.animalType = animalType
    
+   def info(self):
+    print(f"Hye I am {self.imgOfAnimal}. I {self.habit} so, i called as {self.animalType}")
+print("---------WELCOME TO KNOW US--------")
 
-obj1 = animal()
-# print(obj1.name)
-# print(obj1.habit)
-# print(obj1.typeOfAnimal)
-obj1.info()
+# object define lon, elephant, wolf, tiger, goat, cat they are obj of class "animal" 
+lion = animal("🦁", "eat flesh", "carnivours")
+elephant = animal("🐘", "eat plants", "herbivours")
+wolf = animal("🐺", "eat flesh", "carnivours")
+tiger = animal("🐯", "eat fleshh", "carnivours")
+goat = animal("🐐", "eat grass", "herbivours")
+cat = animal("🙀", "eat both", "omnivours")
+
+# method call
+lion.info()
+elephant.info()
+wolf.info()
+cat.info()
+tiger.info()
+goat.info()
+
+
+
+
+
+ 
